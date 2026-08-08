0d993a5ca17e49f2c0f2544af8d852aa58b4c89a
DhilanKhatriFTC <khatridhilan@gmail.com>
2026-08-08 13:35:35 -0400

    Change API key handling in generateResponse function
    Updated the generateResponse function to use a hardcoded API key instead of fetching it from the config file.

--- hf.py
+++ hf.py
@@ -1,8 +1,8 @@
-import config
+#import config
 from huggingface_hub import InferenceClient
 models=getattr(config, "HF_MODELS", ["meta-llama/Llama-3.1-8B-Instruct"])
 def generateResponse(prompt:str,temperature:float=0.3,maxToken:int=512)->str:
-    key=getattr(config,"hfAPIKEY1",None)
+    key="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"
     if not key:
         return "Error, API-Key Missing In Config File"
     lastError=None
