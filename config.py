caf27042a0d8fbdcbf0f47657d63116dfe7fc127
DhilanKhatriFTC <khatridhilan@gmail.com>
2026-08-08 13:38:56 -0400

    Refactor indentation and formatting in config.py

--- config.py
+++ config.py
@@ -4,26 +4,26 @@ import os
 
 try:
 
-import streamlit as st
-
-_secrets = st.secrets
-
+  import streamlit as st
+  
+  _secrets = st.secrets
+  
 except Exception:
-
-_secrets = {}
+  
+  _secrets = {}
 
 def _get(name, default=""):
 
 # look in Streamlit secrets, then environment variables, then a default
 
-if name in _secrets:
-
-return _secrets[name]
-
-return os.environ.get(name, default)
-
+  if name in _secrets:
+  
+    return _secrets[name]
+  
+  return os.environ.get(name, default)
+  
 GROQ_API_KEY = _get("GROQ_API_KEY")
-
+  
 HF_API_KEY = _get("HF_API_KEY")
 
 GROQ_MODELS = ["llama-3.1-8b-instant", "llama-3.3-70b-versatile"]
